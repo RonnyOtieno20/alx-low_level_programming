4c1d8fcfa9692bdcade9f15df09ae0d1dbc68719
RonnyOtieno20 <ronnyotieno20@gmail.com>
2022-09-08 15:07:34 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,19 +3,17 @@
 
 
 /**
- *
+
  *  * main - Entry point
  *
- *   *
+ 
  *
  *    * Description: print a quote using write function
- *
+ 
  *     *    ssize_t write(int fd, const void *buf, size_t count);
- *
- *      *
- *
+ 
  *       * Return: This time we return an error 1.
- *
+
  *       */
 
 
