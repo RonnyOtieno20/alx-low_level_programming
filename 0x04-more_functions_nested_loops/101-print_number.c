b3bc7acd4198edf9d41c081e7765768168f58432
RonnyOtieno20 <ronnyotieno20@gmail.com>
2022-09-16 00:09:43 +0300

    fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,71 +1,26 @@
-#include <stdio.h>
-#include "main.h"
+#include"main.h"
 
 /**
- * exponent - x to the power of y
- * @x: base number
- * @y: exponent
- * Description: calcuates x^y
- * Return: x^y
+ * print_number - Prints an integer.
  *
- **/
+ * @n: The integer to be printed.
+*/
 
-int exponent(int x, int y)
+void print_number(int n)
 {
-	int power;
+	unsigned int num = n;
 
-	power = x;
-
-	if (x == 0)
-		return (0);
-	if (y == 0)
-		return (1);
-
-	while (y >= 2)
-	{
-		power  = power * x;
-		y--;
-	}
-	return (power);
-}
-
-/**
- * print_number - print an int using only _putchar
- * @number: int to be printed by function
- *
- * Return: nothing
- **/
-
-void print_number(int number)
-{
-	int size, digit;
-	long counter, sign;
-
-	sign = 1;
-	digit = 0;
-	size = 1;
-	counter = number;
-
-	if (number < 0)
+	/*first check if its negative*/
+	if (n < 0)
 	{
 		_putchar('-');
-		sign = -1;
-		counter *= sign;
+		num = -num;
 	}
 
-	for (; counter >= 10; size++)
-	{
-		counter = counter / 10;
-	}
+	/*print the first few digits*/
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	counter = sign * (long)number;
-
-	while (size >= 2)
-	{
-		digit = (counter / exponent(10, size - 1));
-		_putchar(digit + '0');
-		counter = counter % exponent(10, size - 1);
-		size--;
-	}
-	_putchar(counter % 10 + '0');
+	/*print the last digit*/
+	_putchar((num % 10) + 48);
 }
