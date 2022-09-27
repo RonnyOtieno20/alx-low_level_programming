cd1e73f1e023f9aae33f26bde7b93d615e071325
RonnyOtieno20 <ronnyotieno20@gmail.com>
2022-09-27 20:44:25 +0000

    fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,29 +1,38 @@
-/**
- * _strchr - a function that locates a character in a string
- *
- * @s: pointer to our string array input
- * @c: character to locate from input array
- *
- * Return: first occurence of charatcer or null if not found
+/*
+ * File: 3-strspn.c
  */
-#include "main.h"
-#include <stdio.h>
 
-char *_strchr(char *s, char c)
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: The number of bytes in s which
+ *         consist only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
 {
-  while (*s != '\0')
-    {
-      if (*s == c)
-	return (s);
-      s++;
-    }
-  /**
-   * if c is '\0', you should return
-   * the pointer to the '\0' of the
-   * string s
-   */
-  if (*s == c)
-    return (s);
-  /*return null if not found*/
-  return ('\0');
+	unsigned int bytes = 0;
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+
+		s++;
+	}
+
+	return (bytes);
 }
