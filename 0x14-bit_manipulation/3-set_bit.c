011bfc9d17b7c93a4190ba232b843a772911632a
RonnyOtieno20 <ronnyotieno20@gmail.com>
2023-09-29 09:26:46 +0300

    hot fix

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,12 +12,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 	/* Check if index is larger than the number of bits */
 	if (index >= (sizeof(*n) * 8))
 		return (-1);
-
-	/* Create a mask with a 1 at the specified index */
-	unsigned long int mask = 1UL << index;
-
 	/* Set the bit at the specified index to 1 */
-	*n |= mask;
+	*n |= 1 << index;
 
 	return (1);
 }
