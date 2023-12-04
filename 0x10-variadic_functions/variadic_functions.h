a3d65ec87b6874d8902298091d5a4cb0f4cad7e8
RonnyOtieno20 <ronnyotieno20@gmail.com>
2023-12-04 14:42:42 +0300

    header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/* standard libary headers */
+#include <stdarg.h> /* Variable arguments handling */
+#include <stdlib.h> /* Standard library functions */
+
+/* function prototypes */
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
