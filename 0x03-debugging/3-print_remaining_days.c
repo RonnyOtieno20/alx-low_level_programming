5fe601baae9b1715e7387d36a2fff3233c111154
= <=>
2024-01-05 20:59:07 +0300

    print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,13 +12,19 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+	int day_of_year;
+
 	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 		if (month >= 2 && day == 60)
 		{
 			day++;
 		}
-		day += 31;
+		day_of_year = day;
+		if (month > 1)
+		{
+			day_of_year += 31;
+		}
 
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
