f40ce032fd80b7bc67f1f15b80f0078a9636d1b6
= <=>
2024-02-16 21:30:06 +0300

    reverses the contents of an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* reverse_array - Reverses the content of an array of integers
+* @a: The array to be reversed
+* @n: The number of elements in the array
+*
+* Return: Nothing
+*/
+void reverse_array(int *a, int n)
+{
+	int i = 0, temp;
+
+	while (i != n / 2)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+		i++;
+	}
+}
