f2936b10d23a675c9c9e51bb5153dc0ef8614975
= <=>
2024-02-19 10:25:08 +0300

    capitalize every word of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,35 @@
-
-#include "main.h"
-
 /**
-* cap_string - capitalizes all words of a string
-* @str: string to be capitalized
-*
-* Return: string
-*/
-char *cap_string(char *str)
+ * cap_string - capitalizes all words of a string.
+ * @s: string.
+ * Return: string.
+ */
+char *cap_string(char *s)
 {
-	int i = 0;
-	int j = 1;
+	int i;
 
-	while (str[i] != '\0')
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-		str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i]
-		== '!' || str[i] == '?' || str[i] == '"' || str[i] == '('
-		|| str[i] == ')' || (str[i] >= '0' && str[i] <= '9'))
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] >= 'a' && s[i] <= 'z'))
 		{
-			j = 1;
+			s[i] = s[i] - 32;
 		}
-		else if (j == 1 && (str[i] >= 'a' && str[i] <= 'z'))
+		else if ((s[0] >= 97 && s[0] <= 122))
 		{
-			str[i] -= 32;
-			j = 0;
+			s[0] = s[0] - 32;
 		}
 		else
 		{
-			j = 0;
+			s[i] = s[i];
 		}
 		i++;
 	}
-	return (str);
+	return (s);
 }
