e065f3db26be7678c208cbf0132517d663fbf6ca
= <=>
2024-02-23 10:43:18 +0300

    prints a string recursively

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
-  * _puts_recursion - function to print a string followed by a newline
-  *
-  * @s: pointer to string
-  */
+* _puts_recursion - prints a string followed by a new line
+* @s: string to be printed
+* Return: Nothing
+*/
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
+		return;
 	else
-		{
-			_putchar(*s);
-			_puts_recursion(s + 1);
-		}
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
