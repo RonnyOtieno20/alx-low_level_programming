0841084c61e7760ac48dc537ec6135aa29ef9a64
= <=>
2024-02-27 21:08:49 +0300

    returns the factorial of a number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
-  * factorial - returns the factorial of a number
-  * @n: number whose factorial is to be returned
-  *
-  * Return: -1 if number is negative, 1 if number is 0
-  */
-
-int factorial(int n)
+* factorial - returns the factorial of a given number
+* @n: number
+* Return: factorial
+*/
+int factorial (int n)
 {
 	if (n < 0)
 		return (-1);
